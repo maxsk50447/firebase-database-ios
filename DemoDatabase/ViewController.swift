c6129db2cd27a3c9f2b73e5c3d083d77c60fe766
Patipol Wangjaitham <patipol.wa@ku.th>
2019-03-01 02:27:36 +0700

    Finish FirebaseDatabase Ios Third Project

--- DemoDatabase/ViewController.swift
+++ DemoDatabase/ViewController.swift
@@ -7,14 +7,50 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var ref:DatabaseReference!
+    
+    var postData = ["Item 1", "Item 2", "Item 3"]
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self
+        tableView.delegate = self
+        // Do any additional setup after loading the view.
+        ref = Database.database().reference()
+        ref.child("Posts").queryOrderedByKey().observe(.childAdded, with: { (snapshot) in
+            if let valueDictionary = snapshot.value as? [AnyHashable:String] {
+                let title = valueDictionary["title"]
+                self.postData.insert(title ?? "", at: 0)
+                
+                self.tableView.reloadData()
+            }
+        })
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return postData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell")
+        
+        cell?.textLabel?.text = postData[indexPath.row]
+        
+        return cell!
+    }
+    
+    /*
+    // MARK: - Navigation
 
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
